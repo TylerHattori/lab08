2ccef1fc81eddd7958aaa5363a88cbed090a5122
Tyler Hattori <63313357+TylerHattori@users.noreply.github.com>
2020-06-05 22:12:00 -0700

    Add files via upload

--- strFuncs.cpp
+++ strFuncs.cpp
@@ -11,8 +11,6 @@ using namespace std;
  * affect your result. 
  */
 bool isAnagram(string s1, string s2){
-  sort(s1.begin(), s1.end());
-  sort(s2.begin(), s2.end());
   for (int i = 0; i < s1.length(); i++)
   {
     if (isspace(s1[i]))
@@ -25,9 +23,21 @@ bool isAnagram(string s1, string s2){
   }
   if (s1.length() != s2.length())
     return false;
+
   for (int i = 0; i < s1.length(); i++)
   {
-    if (s1[i] != s2[i] && (toupper(s1[i]) != toupper(s2[i])))
+      s1[i] = toupper(s1[i]);
+  }
+  for (int i = 0; i < s2.length(); i++)
+  {
+      s2[i] = toupper(s2[i]);
+  }
+  sort(s1.begin(), s1.end());
+  sort(s2.begin(), s2.end());
+
+  for (int i = 0; i < s1.length(); i++)
+  {
+    if (s1[i] != s2[i])
       return false;
   }
   return true;
@@ -37,13 +47,17 @@ bool isAnagram(string s1, string s2){
  * Postcondition: Returns true if s1 is a palindrome, false otherwise
  *You should provide a recursive solution*/
 bool isPalindrome(const string s1){
-  string s = s1;
-  for (int i = 0; i < s.length()/2 - 1; i++)
-  {
-    if (s[i] != s[s.length()-1-i] && (toupper(s[i]) != toupper(s[s.length()-1-i])))
+  if (s1.length() == 1)
+    return true;
+  else if (s1.length() == 2)
+    if (s1[0] == s1[1])
+      return true;
+    else
       return false;
-  }
-  return true;
+    
+  if (s1[0] != s1[s1.length()-1])
+    return false;
+  return isPalindrome(s1.substr(1,s1.length()-2));
 }
 
 
