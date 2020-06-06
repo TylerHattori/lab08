6867fbd882cbe108635e7a1ba2b13cd68280b8ad
Tyler Hattori <63313357+TylerHattori@users.noreply.github.com>
2020-06-05 20:12:27 -0700

    Add files via upload

---
+++ recLinkedListFuncs.cpp
@@ -0,0 +1,24 @@
+#include "linkedList.h"
+#include "linkedListFuncs.h"
+
+
+//head: ptr to a Node * which is the head of a linked list
+//return sum of all values in linked list using a recursive approach
+//if head is null return 0
+int recursiveSum(Node* head) {
+  if (head==0) return 0;
+  double sum;
+  sum = recursiveSum(head->next);
+  return sum + head->data;
+}
+
+//head: ptr to a Node* which is the head of a linked list
+//return the largest value in the linked list using a recursive approach
+//you may assume the list has at least one element
+int recursiveLargestValue(Node* head) {
+  if (head==0) return 0;
+  int max = head->data;
+  double num = recursiveLargestValue(head->next);
+  if (num > max) max = num;
+    return max;
+}
---
+++ strFuncs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include "strFuncs.h"
+using namespace std;
+
+
+/* Precondition: Two valid strings s1 and s2, each containing a mix of alphabets, spaces and punctuations
+ * Post condition: Return true if one string is an anagram of the other string. White spaces, punctuations and
+ * the case for the letters (upper or lower) should not
+ * affect your result. 
+ */
+bool isAnagram(string s1, string s2){
+  sort(s1.begin(), s1.end());
+  sort(s2.begin(), s2.end());
+  for (int i = 0; i < s1.length(); i++)
+  {
+    if (isspace(s1[i]) || isspace(s2[i]) || s2[i] == NULL)
+      continue;
+    if (s1[i] != s2[i])
+      return false;
+  }
+  return true;
+}
+
+/* Precondition: s1 is a valid string that may contain upper or lower case alphabets, no spaces or special characters
+ * Postcondition: Returns true if s1 is a palindrome, false otherwise
+ *You should provide a recursive solution*/
+bool isPalindrome(const string s1){
+  for (int i = 0; i < s1.length()/2 - 1; i++)
+  {
+    if (s1[i] != s1[s1.length()-1-i])
+      return false;
+  }
+  return true;
+}
+
+
